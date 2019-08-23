4d66643b4e3aa4ea62d7651e8d5c133b6b919035
ubervelocity <mihaitu.popescu@gmail.com>
2019-08-23 18:03:02 +0300

    setting and getting propId

--- src/vcapture.py
+++ src/vcapture.py
@@ -3,10 +3,16 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+# Set new resolution
+cap.set(3, 320)
+cap.set(4, 480)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read() # returns true/false depending whether frame is read correctly
-    # print(cap.get(4))
+
+    # Print resolution in terminal    
+    print(cap.get(3))
+    print(cap.get(4))
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
